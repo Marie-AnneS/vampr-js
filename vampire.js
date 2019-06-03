c0da3a1885b1f5efc75ee3ed761ecf004289042c
Marie-AnneS <marie-anne.seim@gmail.com>
2019-06-03 02:12:46 +0000

    adding count

--- vampire.js
+++ vampire.js
@@ -21,40 +21,80 @@ class Vampire {
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-      let numberOfvampires = 0;
-      let currentvampires = this;
-  
-      // climb "up" the tree (using iteration), counting nodes, until no boss is found
-      while (currentvampires.creator) {
-        currentvampires = currentvampires.creator;
-        numberOfvampires++;
-      }
-  
-      return numberOfvampires;
-    
+    let numberOfvampires = 0;
+    let currentvampires = this;
+
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentvampires.creator) {
+      currentvampires = currentvampires.creator;
+      numberOfvampires++;
+    }
+
+    return numberOfvampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
+    return (
+      this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    );
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    let vampire = null;
+    //console.log(this.name);
+    if (this.name === name) {
+      return this;
+    }
+    for (const offspring of this.offspring) {
+      if (!vampire) {
+        vampire = offspring.vampireWithName(name);
+      } else {
+        break;
+      }
+    }
+    return vampire;
+
+    /*    let vamp = []; // 1
+    let nameVamp = [];
+    if (this.name === name) {
+      vamp.push(this); // 2
+    }
+
+    for (const subOffspring of this.offspring) {
+      const vampireWithNameOver = subOffspring.vampireWithName(name); // 3
+      vamp = vamp.concat(vampireWithNameOver);
+    }
+
+    vamp.forEach(element => {
+      if (typeof element == Object) {
+        console.log(`this the good one: ${element.name}`);
+        nameVamp.push(element.name);
+      }
+    });
+     if (vamp === 3) {
+       return nameVamp.join();
+    } else {
+      return null;
+    } */
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-    
+    let total = 0;
+      for (const offspringOne of this.offspring) {
+        offspringOne.totalDescendents;
+        console.log(offspringOne)
+       ;
+      }
+      return ;
   }
 
   // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
-  }
+  get allMillennialVampires() {}
 
   /** Stretch **/
 
@@ -63,10 +103,22 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  closestCommonAncestor(vampire) {}
 }
+/* */
+let rootVampire;
+rootVampire = new Vampire("root");
+offspring1 = new Vampire("andrew");
+offspring2 = new Vampire("sarah");
+offspring3 = new Vampire("c");
+offspring4 = new Vampire("d");
+offspring5 = new Vampire("e");
+rootVampire.addOffspring(offspring1);
+offspring1.addOffspring(offspring2);
+rootVampire.addOffspring(offspring3);
+offspring3.addOffspring(offspring4);
+offspring4.addOffspring(offspring5);
+
+console.log(rootVampire.totalDescendents); 
 
 module.exports = Vampire;
-
